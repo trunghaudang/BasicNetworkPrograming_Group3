9a0aae4b3b1f37253a89b3667ad42efc8d1f2d03
trunghaudang <126368523+trunghaudang@users.noreply.github.com>
2023-04-22 08:27:22 +0700

    Upload file Week6

---
+++ Week6/Week6/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Week6
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnCall = new System.Windows.Forms.Button();
+            this.rtbShow = new System.Windows.Forms.RichTextBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(12, 26);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(572, 27);
+            this.txtUrl.TabIndex = 0;
+            // 
+            // btnCall
+            // 
+            this.btnCall.Location = new System.Drawing.Point(590, 26);
+            this.btnCall.Name = "btnCall";
+            this.btnCall.Size = new System.Drawing.Size(94, 29);
+            this.btnCall.TabIndex = 1;
+            this.btnCall.Text = "CALL";
+            this.btnCall.UseVisualStyleBackColor = true;
+            this.btnCall.Click += new System.EventHandler(this.btnCall_Click);
+            // 
+            // rtbShow
+            // 
+            this.rtbShow.Location = new System.Drawing.Point(12, 70);
+            this.rtbShow.Name = "rtbShow";
+            this.rtbShow.Size = new System.Drawing.Size(776, 618);
+            this.rtbShow.TabIndex = 2;
+            this.rtbShow.Text = "";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(694, 26);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(94, 29);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "CLOSE";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 700);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.rtbShow);
+            this.Controls.Add(this.btnCall);
+            this.Controls.Add(this.txtUrl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox txtUrl;
+        private Button btnCall;
+        private RichTextBox rtbShow;
+        private Button btnClose;
+    }
+}
---
+++ Week6/Week6/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Week6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private async void btnCall_Click(object sender, EventArgs e)
+        {
+            if (txtUrl.Text != "")
+            {
+                using (var client = new HttpClient())
+                {
+                    var response = await Task.Run(() => client.GetAsync(txtUrl.Text));
+                    // Testing API: https://jsonplaceholder.typicode.com/todos
+                    // Try with another API: http://webcode.me/users.json
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        // handle successful response
+                        var responseBody = await Task.Run(() => response.Content.ReadAsStringAsync());
+                        rtbShow.Text = responseBody;
+                    }
+                    else
+                    {
+                        // handle error response                    
+                        if (response.StatusCode == HttpStatusCode.NotFound)
+                        {
+                            // handle 404 Not Found error
+                            rtbShow.Text = "404 Not Found";
+                        }
+                        else if (response.StatusCode == HttpStatusCode.Unauthorized)
+                        {
+                            // handle 401 Unauthorized error
+                            rtbShow.Text = "401 Unauthorized";
+                        }
+                        else
+                        {
+                            // handle other error
+                            var responseBody = await response.Content.ReadAsStringAsync();
+                            rtbShow.Text = $"Error: {response.StatusCode}\n{responseBody}";
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("The URL is invalid, API cannot be called! Please check again!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }    
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Are you sure you want to exit?", "Notification", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            this.Close();
+        }
+    }
+}
